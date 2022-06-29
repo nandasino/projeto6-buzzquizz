384c8ce553c540cf9b0b7233dff5bbd10937e5c3
unknown <fernandamsino@gmail.com>
2022-06-29 20:02:16 -0300

    function tela 3.1 criada

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function limpaTela(){
     document.querySelector(".conteudo").innerHTML= "";
 }
-//telaTeste();
+telaTeste();
 function telaTeste(){
 document.querySelector(".conteudo").innerHTML+=`
 <div class="telaTeste" onclick="tela31()"></div>
@@ -10,5 +10,18 @@ document.querySelector(".conteudo").innerHTML+=`
 function tela31(){
     limpaTela();
     document.querySelector(".conteudo").innerHTML+=`
+    <div class="tela3 tela31">
+        <h1>Comece pelo começo</h1>
+        <div class="caixaTexto">
+            <input type="text" placeholder="Título do seu quizz"/>
+            <input type="text" placeholder="URL da imagem do seu quizz"/>
+            <input type="text" placeholder="Quantidade de perguntas do quizz"/>
+            <input type="text" placeholder="Quantidade de níveis do quizz"/>
+        </div>
+        <button onclick="tela32()"><p>Prosseguir pra criar perguntas</p></button>
+    </div>
     `;
+}
+function tela32(){
+    limpaTela();
 }
