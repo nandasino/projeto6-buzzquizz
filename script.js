f4c7de2e685d5eab0662c31bb0085561f6e7a38e
unknown <fernandamsino@gmail.com>
2022-07-01 17:43:48 -0300

    tela teste apagada

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ function limpaTela(){
     document.querySelector(".conteudo").innerHTML= "";
 }
 
-telaTeste();
-
-function telaTeste(){
-    document.querySelector(".conteudo").innerHTML+=`
-<div class="telaTeste" onclick="tela31()"></div>
-`;
-}
-
 function tela31(){
     limpaTela();
     document.querySelector(".conteudo").innerHTML+=`
