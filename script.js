f9e4b9507d16b9dbbcae0d03799ddb3bf57260cc
Mateus Vall <mateusvm94@gmail.com>
2022-07-02 11:56:42 -0300

    Gerando perguntas da tela 32

--- script.js
+++ script.js
@@ -40,112 +40,63 @@ function tela32(){
     document.querySelector(".conteudo").innerHTML+=`
     <div class="tela3 tela32">
         <h1 class="titulo">Crie suas perguntas</h1>
-        <div class="areaPergunta areaPergunta1">
-            <div class="caixinhaDeTexto"><h1>Pergunta 1</h1><ion-icon name="create" onclick="togglePergunta1()"></ion-icon></div>
-            <div class="caixaTexto caixaTexto1">
-                    <div class="divisao">
-                    <input type="text" placeholder="Texto da pergunta"/>
-                    <input type="text" placeholder="Cor de fundo da pergunta"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Resposta correta</h1>
-                        <input type="text" placeholder="Resposta correta"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Respostas incorretas</h1>
-                        <input type="text" placeholder="Resposta incorreta 1"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 2"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 3"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-            </div>
-        </div>
-        <div class="areaPergunta areaPergunta2">
-            <div class="caixinhaDeTexto"><h1>Pergunta 2</h1><ion-icon name="create" onclick="togglePergunta2()"></ion-icon></div>
-            <div class="caixaTexto caixaTexto2">
-                    <div class="divisao">
-                    <input type="text" placeholder="Texto da pergunta"/>
-                    <input type="text" placeholder="Cor de fundo da pergunta"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Resposta correta</h1>
-                        <input type="text" placeholder="Resposta correta"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Respostas incorretas</h1>
-                        <input type="text" placeholder="Resposta incorreta 1"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 2"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 3"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-            </div>
-        </div>
-        <div class="areaPergunta areaPergunta3">
-            <div class="caixinhaDeTexto"><h1>Pergunta 3</h1><ion-icon name="create" onclick="togglePergunta3()"></ion-icon></div>
-            <div class="caixaTexto caixaTexto3">
-                    <div class="divisao">
-                    <input type="text" placeholder="Texto da pergunta"/>
-                    <input type="text" placeholder="Cor de fundo da pergunta"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Resposta correta</h1>
-                        <input type="text" placeholder="Resposta correta"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <h1 class="subtitulo">Respostas incorretas</h1>
-                        <input type="text" placeholder="Resposta incorreta 1"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 2"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-                    <div class="divisao">
-                        <input type="text" placeholder="Resposta incorreta 3"/>
-                        <input type="text" placeholder="URL da imagem"/>
-                    </div>
-            </div>
-        </div>
-        <button onclick="tela33()">Prosseguir pra criar níveis</button>
     </div>
     `
+
+    geraPerguntas();
 }
 
-function togglePergunta1() {
-    const caixaPergunta1 = document.querySelector('.caixaTexto1');   
-    const aumentaPergunta1= document.querySelector('.areaPergunta1');
+function geraPerguntas(){
+    const tela32 = document.querySelector('.tela32');
+
+    for(let index=1; index<=qtdPerguntas; index++){
+        tela32.innerHTML += 
+        `
+        <div class="areaPergunta">
+            <div class="caixinhaDeTexto"><h1>Pergunta ${index}</h1><ion-icon name="create" onclick="togglePergunta(this)"></ion-icon></div>
+            <div class="caixaTexto">
+                    <div class="divisao">
+                    <input type="text" placeholder="Texto da pergunta"/>
+                    <input type="text" placeholder="Cor de fundo da pergunta"/>
+                    </div>
+                    <div class="divisao">
+                        <h1 class="subtitulo">Resposta correta</h1>
+                        <input type="text" placeholder="Resposta correta"/>
+                        <input type="text" placeholder="URL da imagem"/>
+                    </div>
+                    <div class="divisao">
+                        <h1 class="subtitulo">Respostas incorretas</h1>
+                        <input type="text" placeholder="Resposta incorreta 1"/>
+                        <input type="text" placeholder="URL da imagem"/>
+                    </div>
+                    <div class="divisao">
+                        <input type="text" placeholder="Resposta incorreta 2"/>
+                        <input type="text" placeholder="URL da imagem"/>
+                    </div>
+                    <div class="divisao">
+                        <input type="text" placeholder="Resposta incorreta 3"/>
+                        <input type="text" placeholder="URL da imagem"/>
+                    </div>
+            </div>
+        </div>
+
+        `
+    }
+
+    tela32.innerHTML += 
+    `
+    <button onclick="tela33()">Prosseguir pra criar níveis</button>
+    `;
+
     
-    caixaPergunta1.classList.toggle('mostrar');
-    aumentaPergunta1.classList.toggle('aumentar');
-  }
-  function togglePergunta2() {
-    const caixaPergunta2 = document.querySelector('.caixaTexto2');   
-    const aumentaPergunta2= document.querySelector('.areaPergunta2');
+}
+
+function togglePergunta(elemento) {
+    const areaPergunta = elemento.parentNode.parentNode;
+    const caixaPergunta = areaPergunta.querySelector('.caixaTexto');
     
-    caixaPergunta2.classList.toggle('mostrar');
-    aumentaPergunta2.classList.toggle('aumentar');
-  }
-  function togglePergunta3() {
-    const caixaPergunta3 = document.querySelector('.caixaTexto3');   
-    const aumentaPergunta3= document.querySelector('.areaPergunta3');
-    
-    caixaPergunta3.classList.toggle('mostrar');
-    aumentaPergunta3.classList.toggle('aumentar');
+    caixaPergunta.classList.toggle('mostrar');
+    areaPergunta.classList.toggle('aumentar');
   }
 function tela33(){
     limpaTela();
@@ -301,7 +252,7 @@ function preencheQuizzes(resposta){
     }
 }
 
-criaTela1();
-//tela31();
+//criaTela1();
+tela31();
 
   
